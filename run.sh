6b4a744dba8576b9590b5f270c293381d3fc29ad
Simon Mutch <smutch@unimelb.edu.au>
2022-03-02 11:01:51 +1100

    Improve run.sh wrapper by allowing passing of extra args to Snakemake
    e.g. `./run.sh -n`

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-configfile=config-tmp.yml
+configfile=config/config.yml  # Not necessary if using the default config file, but this way we can override easily
 ncores=1
 
-snakemake -c${ncores} --use-conda --configfile ${configfile} -R `snakemake --list-params-changes --configfile ${configfile}`
+snakemake -c${ncores} --use-conda --configfile ${configfile} $* -R `snakemake --list-params-changes --configfile ${configfile}`
