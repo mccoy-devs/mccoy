d6df9aae22e1103ae3a6609fa25ec2c692ccdea9
Simon Mutch <smutch@unimelb.edu.au>
2022-03-02 12:24:11 +1100

    Add onstart rule

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-configfile=config/config.yml  # Not necessary if using the default config file, but this way we can override easily
-ncores=1
+# Not necessary if using the default config file, but this way we can override easily...
+configfile="${CONFIGFILE:-config/config.yml}"
 
-snakemake -c${ncores} --use-conda --configfile ${configfile} $* -R `snakemake --list-params-changes --configfile ${configfile}`
+snakemake --use-conda --configfile ${configfile} $* -R `snakemake --list-params-changes --configfile ${configfile}`
