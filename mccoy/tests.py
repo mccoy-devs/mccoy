967b8a6a5f4a8318413b94c7c30ec40d1b8dd514
Wytamma Wirth <wytamma.wirth@me.com>
2022-06-15 12:44:42 +1000

    :art: pre-commit

--- mccoy/tests.py
+++ mccoy/tests.py
@@ -42,7 +42,7 @@ def test_tree(tree: Tree):
 
 def test_root_to_tip(tree: Tree):
     """
-    Test the tree for temporal signal with a root-to-tip regression. 
+    Test the tree for temporal signal with a root-to-tip regression.
     https://phytest-devs.github.io/phytest/reference.html#phytest.bio.tree.Tree.assert_root_to_tip
     """
-    tree.assert_root_to_tip(min_r_squared=0)  # ensure RTTR is positive 
+    tree.assert_root_to_tip(min_r_squared=0)  # ensure RTTR is positive
