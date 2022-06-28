13f39643d23a52f635df128d7c268e1e9eb61568
Simon Mutch <smutch@unimelb.edu.au>
2022-06-28 15:48:53 +1000

    :tv: Update noxfile to use nox-poetry

--- noxfile.py
+++ noxfile.py
@@ -1,19 +1,21 @@
 import nox
+from nox_poetry import session
 
 nox.options.sessions = ["test"]
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
+@session(python=["3.7", "3.8", "3.9", "3.10"])
 def test(session):
-    session.run("poetry", "install", "-v", external=True)
-    session.run("poetry", "run", "pytest", external=True)
+    session.install("pytest")
+    session.run("pytest")
 
 
-@nox.session
+@session
 def style(session):
     session.run("pre-commit", "run", "--all-files", "--show-diff-on-failure", external=True)
 
 
-@nox.session
+@session
 def docs(session):
-    session.run("poetry", "run", "sphinx-build", "docs", "docs/_build/html", external=True)
+    session.install("sphinx", "sphinx-rtd-theme", "myst-parser", "sphinx-copybutton")
+    session.run("sphinx-build", "docs", "docs/_build/html")
