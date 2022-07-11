3a03935fb8cf7e1f772bc45231183ddc9c77703e
Simon Mutch <smutch@unimelb.edu.au>
2022-07-11 10:23:13 +1000

    :cop: use dendropy to test tree distance for iqtree tests

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,6 +1,15 @@
-import pytest
+import dendropy
 
 
-@pytest.mark.skip(reason="The current test dataset leads to unstable results (even with same random seed).")
 def test_tree(run_workflow):
-    run_workflow("results/tree/expected-1.fasta.bionj").assert_expected()
+    treefile = "results/tree/expected-1.fasta.treefile"
+    workflow = run_workflow(treefile)
+
+    tns = dendropy.TaxonNamespace()
+    tree1 = dendropy.Tree.get_from_path(workflow.work_dir / treefile, "newick", taxon_namespace=tns)
+    tree2 = dendropy.Tree.get_from_path(workflow.expected_dir / treefile, "newick", taxon_namespace=tns)
+    tree1.encode_bipartitions()
+    tree2.encode_bipartitions()
+
+    distance = dendropy.calculate.treecompare.euclidean_distance(tree1, tree2)
+    assert distance <= 1
