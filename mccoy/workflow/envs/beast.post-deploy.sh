bd86ca6d027821f01a06c8590d27e400e1e13642
Simon Mutch <smutch@unimelb.edu.au>
2022-08-04 16:22:13 +1000

    Fix beast env post-deploy script

--- mccoy/workflow/envs/beast.post-deploy.sh
+++ mccoy/workflow/envs/beast.post-deploy.sh
@@ -1,3 +1,4 @@
 packagemanager -list \
-    | awk '{if($1 ~ /feast/ && $3 ~ /NA/){exit 0} else {exit 1}}' \
-    && packagemanager -add feast || true
+  | awk '{if($1 ~ /feast/){if($3 ~ /NA/){exit 1}}}' \
+  && echo "feast is already installed" \
+  || packagemanager -add feast
