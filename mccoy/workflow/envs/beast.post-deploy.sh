87d7864f36f49f5ac44f47cd4f1757d2c59b1325
Simon Mutch <smutch@unimelb.edu.au>
2022-09-16 10:25:30 +1000

    :bug: Allow beast post-deploy script to succeed if feast already installed

--- mccoy/workflow/envs/beast.post-deploy.sh
+++ mccoy/workflow/envs/beast.post-deploy.sh
@@ -1,2 +1,2 @@
 beast -version  # Need to call beast once (even just to query the version) to create support dirs
-packagemanager -add feast
+packagemanager -add feast | grep -e 'feast is \(already \)\?installed'
